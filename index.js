f9bc9db8ff1ba5d67837b8a99f97bd5dd717e83d
Justin L Rackham <justin.rackham@gmail.com>
2019-06-06 07:34:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
+// Having trouble getting ${name} to call 'you'. Directions seem to be different. Initial thoughts were to call the function... happyHolidaysTo('you') outside of brackets
 function happyHolidaysTo(name)  {
  
   return 'Happy holidays, you!'
   
+}
+
+function happyHolidaysTo(holiday, name) {
+  return "Happy birthday, Justin!"
 }
