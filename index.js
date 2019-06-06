4b533b61657a7526f0f181c72874adc2f097c50e
Justin L Rackham <justin.rackham@gmail.com>
2019-06-06 07:41:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,18 @@ function holidayCountdown(holiday, days) {
 }
 */
 
+function happyHolidays () {
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo (name) {
+  return `Happy holidays, ${name}!`;
+}
+
+function happyHolidayTo (holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown (holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
+}
