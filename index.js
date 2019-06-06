90482139cf505aabc1e7d0731ff7b30af5fdd970
Justin L Rackham <justin.rackham@gmail.com>
2019-06-06 07:37:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ function happyHolidaysTo(name)  {
 function happyHolidayTo(holiday, name) {
   return "Happy Independence Day, you!"
 }
+
+function holidayCountdown(holiday, days) {
+  return "It's ten days until Christmas!"
+}
